df47451980c2f5e786375247a5568811af566c80
payman nosraty <nosratypayman@gmail.com>
2020-09-01 09:55:24 +0430

    Add project files.

---
+++ Models/Base/Entity.cs
@@ -0,0 +1,31 @@
+﻿namespace Models.Base
+{
+	public abstract class Entity : object
+	{
+		public Entity() : base()
+		{
+			Id =
+				System.Guid.NewGuid();
+
+			InsertDateTime = System.DateTime.Now;
+		}
+
+		// **********
+		/// <summary>
+		/// شماره منحصر به فرد
+		/// </summary>
+		[System.ComponentModel.DataAnnotations.Required
+			(AllowEmptyStrings = false)]
+		public System.Guid Id { get; set; }
+		// **********
+
+		// **********
+		/// <summary>
+		/// تاریخ و زمان درج اطلاعات
+		/// </summary>		
+		[System.ComponentModel.DataAnnotations.Required
+			(AllowEmptyStrings = false)]
+		public System.DateTime InsertDateTime { get; set; }
+		// **********
+	}
+}
---
+++ Models/DatabaseContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+	public class DatabaseContext : System.Data.Entity.DbContext
+	{
+		static DatabaseContext()
+		{
+			System.Data.Entity.Database.SetInitializer(new DatabaseContextInitializer());
+		}
+
+		public DatabaseContext() : base()
+		{
+		}
+	}
+}
